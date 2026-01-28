1f9deee7c1168408d57763f2a8da1659770a398e
JLopezore <j.lopezlopez1004@gmail.com>
2026-01-28 14:00:53 -0600

    fix: change port CORS

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:8100"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
